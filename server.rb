37bc44ccbb6a5a17ac33ab99c51d381c38cb9594
Serge Lebedev <angly.cat@gmail.com>
2019-06-21 10:28:49 +0600

    Provide OAuth callback

--- server.rb
+++ server.rb
@@ -1,7 +1,27 @@
 require 'sinatra'
+require 'rest-client'
+require 'json'
 
 CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 
 get '/login' do
   erb :login, :locals => {:client_id => CLIENT_ID}
+end
+
+get '/callback' do
+  # get temporary GitHub code...
+  session_code = request.env['rack.request.query_hash']['code']
+
+  # ... and POST it back to GitHub
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+                           {:client_id => CLIENT_ID,
+                            :client_secret => CLIENT_SECRET,
+                            :code => session_code},
+                            :accept => :json)
+
+  # extract the token
+  access_token = JSON.parse(result)['access_token']
+
+  puts access_token
 end
