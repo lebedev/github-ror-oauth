a900276503b9182e15b293ea52b4f1a55b24d19a
Serge Lebedev <angly.cat@gmail.com>
2019-06-21 10:23:03 +0600

    Implement login page

--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
-get '/' do
-  "Hello world, it's #{Time.now} at the server!"
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+
+get '/login' do
+  erb :login, :locals => {:client_id => CLIENT_ID}
 end
